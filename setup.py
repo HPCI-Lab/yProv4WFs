769ef853760773de10f5726fac01054503565dc3
Carolina <carolina.sopranzetti@studenti.unitn.it>
2024-10-29 20:25:31 +0100

    module yprov4wfs-v0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='yprov4wfs',                    
+    name='yProv4WFs',                    
     version='0.0.2',                     
     packages=find_packages(),           
     install_requires=[],
